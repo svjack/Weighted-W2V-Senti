d8cad8ed4a80b5bd254ba5d28c3e8784d6dbaca5
svjack <ehangzhou@outlook.com>
2020-08-26 18:01:24 +0800

    upload

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 26 15:31:50 2020
+
+@author: svjack
+"""
+
+kaggle datasets download utmhikari/doubanmovieshortcomments
+mkdir data
+unzip doubanmovieshortcomments.zip -d data
